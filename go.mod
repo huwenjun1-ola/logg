a249f3f7e3a454b7c4ca3fbfd1d5d8402df30daf
胡文俊(Hu WenJun/星云) <hu.wenjun1@olaola.chat>
2024-04-08 14:27:39 +0800

    log4go

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/huwenjun1-ola/logg
+
+go 1.20
+
+require github.com/alecthomas/log4go v0.0.0-20180109082532-d146e6b86faa // indirect
---
+++ log4g.go
@@ -0,0 +1,13 @@
+package logg
+
+import "github.com/alecthomas/log4go"
+
+func Info(arg0 interface{}, args ...interface{}) {
+	log := log4go.NewLogger()
+	defer log.Close()
+	log.AddFilter("stdout", log4go.DEBUG, log4go.NewConsoleLogWriter())
+	log4go.Info(arg0, args...)
+}
+func Version() string {
+	return "log4go"
+}
